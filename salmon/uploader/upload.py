5cf844bea1bf2afecbdea2c86e1847fdc35e132c
miandru <156494934+miandru@users.noreply.github.com>
2025-03-28 14:06:36 +0100

    squash update master (#12)
    * Fix mangled spectrals when file renaming was occuring
    
    This was happening when initial files were named 1. xx, 2. xx, up to 10. xx
    Lexical sorting was used, making the spectrals ids 1, 10, 2...
    
    If file renaming was occuring, the spectrals uploaded on torrent description
    were not matching the actual files.
    
    * Improve cosmetics between steps
    
    * Add some prettier icons for FLAC/itunes/qobuz
    
    * Fix JunoDownload scraping
    
    * Fix (kinda) itunes scraping
    
    Still broken for artists, explicit tag and discnumber
    
    ---------
    
    Co-authored-by: redusys <redusys.ops@gmail.com>

--- salmon/uploader/upload.py
+++ salmon/uploader/upload.py
@@ -34,6 +34,7 @@ def prepare_and_upload(
     hybrid,
     lossy_master,
     spectral_urls,
+    spectral_ids,
     lossy_comment,
     request_id,
     source_url
@@ -49,6 +50,7 @@ def prepare_and_upload(
             hybrid,
             cover_url,
             spectral_urls,
+            spectral_ids,
             lossy_comment,
             request_id,
             source_url=source_url
@@ -61,6 +63,7 @@ def prepare_and_upload(
             track_data,
             hybrid,
             spectral_urls,
+            spectral_ids,
             lossy_comment,
             request_id,
             source_url=source_url
@@ -94,6 +97,7 @@ def compile_data_new_group(
     hybrid,
     cover_url,
     spectral_urls,
+    spectral_ids,
     lossy_comment,
     request_id=None,
     source_url=None
@@ -127,7 +131,7 @@ def compile_data_new_group(
         "image": cover_url,
         "album_desc": generate_description(track_data, metadata),
         "release_desc": generate_t_description(
-            metadata, track_data, hybrid, metadata["urls"], spectral_urls, lossy_comment, source_url
+            metadata, track_data, hybrid, metadata["urls"], spectral_urls, spectral_ids, lossy_comment, source_url
         ),
         'requestid': request_id,
     }
@@ -140,6 +144,7 @@ def compile_data_existing_group(
     track_data,
     hybrid,
     spectral_urls,
+    spectral_ids,
     lossy_comment,
     request_id,
     source_url=None
@@ -162,7 +167,7 @@ def compile_data_existing_group(
         "vbr": metadata["encoding_vbr"],
         "media": metadata["source"],
         "release_desc": generate_t_description(
-            metadata, track_data, hybrid, metadata["urls"], spectral_urls, lossy_comment, source_url
+            metadata, track_data, hybrid, metadata["urls"], spectral_urls, spectral_ids, lossy_comment, source_url
         ),
         'requestid': request_id,
     }
@@ -262,7 +267,7 @@ def generate_description(track_data, metadata):
 
 
 def generate_t_description(
-    metadata, track_data, hybrid, metadata_urls, spectral_urls, lossy_comment, source_url
+    metadata, track_data, hybrid, metadata_urls, spectral_urls, spectral_ids, lossy_comment, source_url
 ):
     """
     Generate the torrent description. Add information about each file, and
@@ -270,21 +275,26 @@ def generate_t_description(
     """
     description = ""
     if spectral_urls:
-        description += make_spectral_bbcode(list(track_data.keys()), spectral_urls)
+        description += make_spectral_bbcode(spectral_ids, spectral_urls)
 
     if not hybrid:
         track = next(iter(track_data.values()))
         if track["precision"]:
-            description += "Encode Specifics: {} bit {:.01f} kHz\n".format(
+            if config.ICONS_IN_DESCRIPTIONS:
+                description += "[img]https://ptpimg.me/pu93q2.png[/img]"
+            else:
+                description += "Encode Specifics:"
+            description += " [b]{} bit [color=#2E86C1]{:.01f}[/color] kHz[/b]".format(
                 track["precision"], track["sample rate"] / 1000
             )
+            description += "\n"
         else:
             description += "Encode Specifics: {:.01f} kHz\n".format(
                 track["sample rate"] / 1000
             )
 
     if metadata["date"]:
-        description += f'Released on {metadata["date"]}\n'
+        description += f'Released on [b]{metadata["date"]}[/b]\n'
 
     if config.INCLUDE_TRACKLIST_IN_T_DESC or hybrid:
         for filename, track in track_data.items():
